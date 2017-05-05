c26266c509eabfcf1e9ade6f6fa545b6e97964ae
Oleh Makhobei <tajcig@ya.ru>
2017-05-05 11:56:14 +0300

    first commit

--- MyOpenGl/ViewController.m
+++ MyOpenGl/ViewController.m
@@ -16,14 +16,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    GLKView *view = (GLKView *) self.view;
+    view.context = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void) glkView:(GLKView *)view drawInRect:(CGRect)rect{
+    glClearColor(1.0, 0, 1.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
+
 
 
 @end
