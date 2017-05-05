bfcc4c316847df95b9c7bf426b890b33a22537ce
Oleh Makhobei <tajcig@ya.ru>
2017-05-05 10:22:43 +0300

    Initial Commit

---
+++ MyOpenGl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyOpenGl
+//
+//  Created by Oleh Makhobei on 5/5/17.
+//  Copyright © 2017 Oleh Makhobei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyOpenGl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyOpenGl
+//
+//  Created by Oleh Makhobei on 5/5/17.
+//  Copyright © 2017 Oleh Makhobei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
